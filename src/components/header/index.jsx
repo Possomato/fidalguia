5ce79672770586ec07cfb59297e329d8e19b1cca
Gabriel Bonifacio Possomato <gabrielb.possomato@gmail.com>
2023-08-23 14:57:39 -0300

    add header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,9 +1,21 @@
-import {Container} from './style'
+import {RiShutDownLine} from 'react-icons/ri'
+
+import {Container, Profile, Logout} from './style'
 
 export function Header(){
   return(
     <Container>
-      
+      <Profile>
+        <img src="https://github.com/possomato.png" alt="foto do usuário" />
+
+        <div>
+          <span>Bem-vindo,</span>
+          <strong>Gabriel Possomato</strong>
+        </div>
+      </Profile>
+      <Logout>
+        <RiShutDownLine />
+      </Logout>
     </Container>
   )
 }
