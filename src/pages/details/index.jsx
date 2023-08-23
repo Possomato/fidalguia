4dacd6a16c5faa6eb4ed938286a784aa42ff1bf9
Gabriel Bonifacio Possomato <gabrielb.possomato@gmail.com>
2023-08-23 20:05:26 -0300

    att details

--- src/pages/details/index.jsx
+++ src/pages/details/index.jsx
@@ -1,4 +1,4 @@
-import { Container, Links } from './style'
+import { Container, Links, Content } from './style'
 
 import { Header } from '../../components/header'
 import { Button } from '../../components/button/'
@@ -11,21 +11,35 @@ export function Details(){
     <Container>
       <Header />
 
-      <ButtonText title='excluir nota' />
+      <main>
+        <Content>
 
-      <Section title='Links úteis'>
-        <Links>
-          <li><a href="#">Os irmãos Karamazov</a></li>
-          <li><a href="#">Crime e Castigo</a></li>
-        </Links>
-      </Section>
+          <ButtonText title='excluir nota' />
 
-      <Section title='marcadores'>
-        <Tag title='Literatura'></Tag>
-        <Tag title='Biografia'></Tag>
-      </Section>
+          <h1>Fiódor Dostoiévski</h1>
 
-      <Button title="Voltar" />
+          <p>
+          Dostoiévski começou a escrever durante seu exílio na Sibéria. Seu primeiro romance, "Gente Pobre" (1846), já apresentava traços de sua escrita distintiva, com uma abordagem profunda dos sentimentos e motivações dos personagens. No entanto, foi com "Crime e Castigo" (1866) que ele alcançou o reconhecimento internacional. A história do atormentado Raskólnikov explorou temas de culpa, redenção e moralidade, cativando leitores com sua narrativa intensa e análise psicológica.
+
+          Dostoiévski continuou a criar obras-primas literárias, incluindo "O Idiota" (1869), "Os Demônios" (1872) e "Os Irmãos Karamazov" (1880), esta última frequentemente considerada sua obra-prima. Nestas obras, ele mergulhou nas profundezas da mente humana, examinando questões filosóficas e religiosas, bem como os dilemas morais que moldam nossas escolhas.
+          </p>
+
+          <Section title='Links úteis'>
+            <Links>
+              <li><a href="#">Os irmãos Karamazov</a></li>
+              <li><a href="#">Crime e Castigo</a></li>
+            </Links>
+          </Section>
+
+          <Section title='marcadores'>
+            <Tag title='Literatura'></Tag>
+            <Tag title='Biografia'></Tag>
+          </Section>
+
+          <Button title="Voltar" />
+
+        </Content>
+      </main>
     </Container>
   )
 }
--- src/pages/details/style.js
+++ src/pages/details/style.js
@@ -9,6 +9,12 @@ export const Container = styled.div`
   grid-template-areas:
   "header"
   "content";
+
+  > main{
+    grid-area: content;
+    overflow-y: scroll;
+    padding: 64px 0;
+  }
 `
 
 export const Links = styled.ul`
@@ -20,5 +26,29 @@ export const Links = styled.ul`
     a{
       color: ${({theme}) => theme.COLORS.WHITE};
     }
+  }
+`
+
+export const Content = styled.div`
+  max-width: 550px;
+  margin: 0 auto;
+
+  display: flex;
+  flex-direction: column;
+
+  > button:first-child{
+    align-self: end;
+  }
+
+  > h1{
+    font-family: 'Roboto', sans-serif;
+    font-weight: 500;
+    padding-top: 64px;
+  }
+
+  > p{
+    font-size: 1rem;
+    margin-top: 1rem;
+    text-align: justify;
   }
 `
