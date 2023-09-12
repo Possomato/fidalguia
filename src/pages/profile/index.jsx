60b9f48adefec68a00ab2bb495affeccf437ce3f
Gabriel Bonifacio Possomato <gabrielb.possomato@gmail.com>
2023-09-12 10:23:39 -0300

    att profile

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -1,5 +1,8 @@
+import { useState } from 'react'
 import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from 'react-icons/fi'
 import { Link } from 'react-router-dom'
+
+import { useAuth } from '../../hooks/auth'
 
 import { Input } from '../../components/input'
 import { Button } from '../../components/button'
@@ -7,6 +10,12 @@ import { Button } from '../../components/button'
 import { Container, Form, Avatar } from './style.js'
 
 export function Profile(){
+  const { user } = useAuth()
+  const [name, setName] = useState(user.name)
+  const [email, setEmail] = useState(user.email)
+  const [passwordOld, setPasswordOld] = useState()
+  const [passwordNew, setPasswordNew] = useState()
+
   return(
     <Container>
       <header>
@@ -34,24 +43,30 @@ export function Profile(){
           placeholder="nome"
           type="text"
           icon={FiUser}
+          value={name}
+          onChange={e => setName(e.target.value)}
         />
 
         <Input
           placeholder="e-mail"
           type="text"
           icon={FiMail}
+          value={email}
+          onChange={e => setEmail(e.target.value)}
         />
 
         <Input
           placeholder="senha atual"
           type="password"
           icon={FiLock}
+          onChange={e => setPasswordOld(e.target.value)}
         />
 
         <Input
           placeholder="senha nova"
           type="password"
           icon={FiLock}
+          onChange={e => setPasswordNew(e.target.value)}
         />
 
         <Button title="Salvar" />
