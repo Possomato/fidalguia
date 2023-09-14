7ca476d5fe0446b6bcdd7f42c37f1f50cd373697
Gabriel Bonifacio Possomato <gabrielb.possomato@gmail.com>
2023-09-14 20:38:14 -0300

    add selected tags

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -12,6 +12,18 @@ import { Note } from '../../components/note'
 
 export function Home(){
   const [tags, setTags] = useState([])
+  const [tagsSelected, setTagsSelected] = useState([])
+
+  function handleTagSelected(tagName){
+    const alreadySelected = tagsSelected.includes(tagName)
+
+    if(alreadySelected){
+      const filteredTags = tagsSelected.filter(tag => tag !== tagName)
+      setTagsSelected(filteredTags)
+    } else{
+      setTagsSelected(prevState => [...prevState, tagName])
+    }
+  }
 
   useEffect(() => {
     async function fetchTags(){
@@ -33,14 +45,19 @@ export function Home(){
          <li>
           <ButtonText
             title='TODOS'
-            isActive
+            onClick={() => handleTagSelected(todos)}
+            isActive={tagsSelected.length === 0}
           />
         </li>
 
         {
           tags && tags.map(tag => (
             <li key={String(tag.id)}>
-              <ButtonText title={tag.name} />
+              <ButtonText
+                title={tag.name}
+                onClick={() => handleTagSelected(tag.name)}
+                isActive={tagsSelected.includes(tag.name)}
+              />
             </li>
           ))
         }
