8db930289e34eef932b8e188877319a1f2ab238f
Gabriel Bonifacio Possomato <gabrielb.possomato@gmail.com>
2023-09-14 08:09:30 -0300

    add remove links on notes

--- src/pages/new/index.jsx
+++ src/pages/new/index.jsx
@@ -19,6 +19,11 @@ export function New(){
     setLinks(prevState => [...prevState, newLink])
     setNewLink('')
   }
+
+  function handleRemoveLink(deleted){
+    setLinks(prevState => prevState.filter(link => link !== deleted))
+  }
+
   return(
     <Container>
       <Header />
@@ -40,7 +45,7 @@ export function New(){
                 <NoteItem
                   key={String(index)}
                   value={link}
-                  onClick={() => {}}
+                  onClick={() => handleRemoveLink(link)}
                 />
               ))
             }
